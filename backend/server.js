61e889422a3e53f0c812afc4929ecb04259ae955
Pooja Patel <poojaclass1994@gmail.com>
2026-08-08 22:25:59 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 // Routes
 app.use("/api/users", authRoutes);
